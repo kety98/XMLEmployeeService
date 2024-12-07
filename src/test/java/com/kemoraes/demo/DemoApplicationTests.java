f72cc5566676ff50629b0b0e8920cd8b18e0fe88
Ketlyn Moraes Campos <kemoraes@unirioja.es>
2024-12-07 12:42:06 -0300

    Project created

---
+++ src/test/java/com/kemoraes/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kemoraes.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
