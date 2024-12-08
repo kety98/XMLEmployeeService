1879b5a9a3a2532ca93641533ba83bc785cd2e95
Ketlyn Moraes Campos <kemoraes@unirioja.es>
2024-12-08 10:56:08 -0300

    Export employee XML

---
+++ src/main/java/com/kemoraes/demo/entities/EmployeesWrapper.java
@@ -0,0 +1,31 @@
+package com.kemoraes.demo.entities;
+
+import java.util.List;
+
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement(name = "employees")
+public class EmployeesWrapper {
+    private List<Employee> employees;
+
+    public EmployeesWrapper() {
+    }
+
+	public EmployeesWrapper(List<Employee> employees) {
+		super();
+		this.employees = employees;
+	}
+
+	@XmlElement(name = "employee")
+	public List<Employee> getEmployees() {
+	    return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+
+    
+}
--- src/main/java/com/kemoraes/demo/resources/EmployeeResource.java
+++ src/main/java/com/kemoraes/demo/resources/EmployeeResource.java
@@ -2,6 +2,9 @@ package com.kemoraes.demo.resources;
 
 import java.util.List;
 
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.Marshaller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kemoraes.demo.entities.Employee;
+import com.kemoraes.demo.entities.EmployeesWrapper;
 import com.kemoraes.demo.services.EmployeeService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping(value = "/employees")
@@ -30,5 +36,20 @@ public class EmployeeResource {
 		Employee obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@GetMapping("/export")
+    public void exportToXml(HttpServletResponse response) throws Exception {
+        List<Employee> employees = service.findAll();
+
+        JAXBContext context = JAXBContext.newInstance(EmployeesWrapper.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        EmployeesWrapper wrapper = new EmployeesWrapper(employees);
+
+        response.setContentType("application/xml");
+        response.setHeader("Content-Disposition", "attachment; filename=employees.xml");
+        marshaller.marshal(wrapper, response.getOutputStream());
+    }
 	
 }
